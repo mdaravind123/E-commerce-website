8498f5854c81db56ed662f3109de4c4a58107431
Aravind <mdaravind2004@gmail.com>
2024-08-30 18:28:06 +0530

    init: project initialization with vite typescript tailwindcss setup complete

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
